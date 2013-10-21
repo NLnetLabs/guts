5556018b806947523d08ba0f02d847f5490cbbbd
Warwick Louw <Warwick@nlnetlabs.nl>
2013-10-21 15:09:05 +0200

    Restructure and general cleaning to improve readability. Next comes modification.

--- Measurement_db_writer.py
+++ Measurement_db_writer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #Author Warwick Louw
-
+## imports
 import os
 import sys
 import json
@@ -14,18 +14,182 @@ import datetime
 import Database
 from datetime import date
 import db_creator
+##
+## The following script is seperated into two:
 
-## sort auxiliary files from callable files
+## Auxiliary methods:
+## These methds are used to complete tasks for members such as return rows, ids, processing etc..
+## These are then seperated: Database specific, Measurement specific, processing and other
+
+## ---------------------------------------------------------------------------------------------------------
+## Database specific auxiliary methods.
+## ---------------------------------------------------------------------------------------------------------
 
 ## return a connection to the database
 def ret_con():
 	con = Database.ret_con()
 	return con
 
-## Anon func to determine whether a key is in a dict.
-## Lengthly name to ensure it does not clash.
-exists_in_dict = lambda x,d: True if x in d else False
+## Return all the measurement ids stored in the database
+def get_all_measurement_ids():
+	ids = []
+	cursor.execute("select mes_msm_id from tbl_Measurements")
+	c = cursor.fetchall()
+	for i in c:
+		ids.append(i[0])
+	return ids
 
+## ~Deprecated~ Keeping for keeps sake
+## Return the last row in a table
+def get_last_row(tbl):
+	cursor.execute('select * from '+tbl+' where oid = (select max(oid) from '+tbl+');')
+	return cursor.fetchone()
+
+## Remove all keys where the values are None
+def sanitize_dict(d):
+	delkeys = []
+	for k,v in d.iteritems():
+		if v is None:
+			delkeys.append(k)
+	for i in range(len(delkeys)):
+		del d[delkeys[i]]
+	return d
+
+## Return the last error id so that we can assign the next one, if there is another error :?
+def get_last_err_id():
+	cursor.execute("Select max(res_err_id) from tbl_results")
+	row = cursor.fetchone()
+	if row[0] is None:
+		return 0
+	else:
+		return row[0]
+
+## Return the last result id so that we can assign the next one
+def ret_max_res_id():
+	cursor.execute("Select max(res_set_id) from tbl_results")
+	row = cursor.fetchone()
+	if row[0] is None:
+		return 0
+	else:
+		return row[0]
+
+## ~Deprecated~ deleted in next commit
+## Return the last icmpext id so that we can assign the next one
+def get_last_icmpext_id():
+	cursor.execute("Select max(set_icmpext_mpls_id) from tbl_result_set")
+	row = cursor.fetchone()
+	if row[0] is None:
+		return 0
+	else:
+		return row[0]
+
+## ~Deprecated~ deleted in next commit
+## Return the last icmpext mpls id so that we can assign the next one
+def get_last_icmpext_mpls_id():
+	cursor.execute("Select max(mpl_icmptext_mpls_id) from tbl_traceroute_mpls")
+	row = cursor.fetchone()
+	if row[0] is None:
+		return 0
+	else:
+		return row[0]
+
+## view all items on the specified table
+def view_tbl(tbl):
+	cursor = Database.ret_con.cursor()## returns a connection which we can then assign a cursor to.
+	rows = cursor.execute("Select * from "+tbl)
+	for row in rows:## to add: include column names and tab everything into place to imporve readability
+		print row
+	#return row.fetchall() ## ~Deprecated~ deleted in next commit
+
+## Inserts data into the database, a tuple(table name, values) is parsed
+def insert_tuple_in_db(inserts):
+	if type(inserts) != type(tuple):
+		print "Inserts should be in the form of a tuple. tuple : (table name, values)"
+		print 'example: ("tbl_Measurements",(111111,"date","127.0.0.1",4,"ICMP","Ping"))'
+		return
+	## to include: split executes larger than 30 000
+	for insert in inserts:
+		## Declarations
+		tbl = insert[0]
+		que = insert[1]
+		##
+		que = sanitize_dict(que)
+		if len(que) >= 1:
+			keys = ", ".join(que.keys())
+			vals = ", ".join('?' * len(que))
+			query = "insert into "+tbl+"({}) values({})".format(keys,vals)
+			cursor.execute(query,que.values())
+
+## Return the table schema as a dict(column name:None)
+def get_tbl_schema(tbl):
+	d = {}
+	for r in cursor.execute("PRAGMA table_info("+tbl+");"):
+		d.update({str(r[1]):None})
+	return d
+
+## ---------------------------------------------------------------------------------------------------------
+## Measurement specific auxiliary methods.
+## ---------------------------------------------------------------------------------------------------------
+
+## Return the data from the measurements file
+def get_measurements_file(fileName):
+	if len( fileName ) > 0:
+		if os.path.exists(fileName):
+			f = open(fileName,'r')
+			mes = f.readlines()
+			f.close()
+			return mes
+	else:
+		print ("Could not find the measurements file: "+str(filename)+" or the file is empty")
+		sys.exit(0)
+
+## Add measurements info to insert list. return tuple for tuple insert.
+def insert_measurement_info(mes_info):
+	tbl_Measurements = get_tbl_schema("tbl_Measurements")
+	for k,v in tbl_Measurements.iteritems():
+		val = mes_info.get(k[:4],None)
+		if val:
+			tbl_Measurements[k] = val
+	return ("tbl_Measurements",tbl_Measurements)
+
+## Return the results of a specific measurement
+def get_measurements_web(measurement):
+	try:
+		urls = "https://atlas.ripe.net/api/v1/measurement/"+str(measurement)+"/result/"
+		req = urllib2.urlopen(urls)
+		req = json.load(req)
+		return req
+	except urllib2.HTTPError, e: ## <- Idea from Willem Toorop
+		print (e.read())
+
+## View measurement info
+def view_measurement_info(measurement):
+	response = measurement_info(measurement)
+	if response != None:
+		## For now, this will do.
+		for k,v in response.iteritems():
+			print k,":",v
+
+## Return the time stamp in the format yyyy-mm-dd/hh-mm-ss
+## Useful for converting a start_time and end_time of a measurement
+## Found on Stackoverflow
+def ret_timestamp_to_dt(ts):
+	dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d/%H:%M:%S')
+	return dt
+
+## Gather Measurements info
+def get_measurement_info(measurement):
+	try:
+		response = urllib2.urlopen("https://atlas.ripe.net/api/v1/measurement/"+str(measurement)).read()
+		response = json.loads(response)
+		return response
+	except urllib2.HTTPError, e: ## <- Idea from Willem Toorop
+		print (e.read())
+		return None
+
+## ---------------------------------------------------------------------------------------------------------
+## Processing for storage.
+## ---------------------------------------------------------------------------------------------------------
 ## Place the data from the web into the database
 def proc_result(results):#change to results
 	try:
@@ -166,141 +330,13 @@ def proc_trace(line,tbls,res_id,err_id,inserts): ## Tested, ugly but it works
 				tbl_set_copy['set_err_value'] = ev
 		inserts.append((tbls[1],tbl_set))
 
-## Remove all keys where the values are None
-def sanitize_dict(d):
-	delkeys = []
-	for k,v in d.iteritems():
-		if v is None:
-			delkeys.append(k)
-	for i in range(len(delkeys)):
-		del d[delkeys[i]]
-	return d
+## ---------------------------------------------------------------------------------------------------------
+## Other:
+## ---------------------------------------------------------------------------------------------------------
 
-## Return the last error id so that we can assign the next one, if there is another error :?
-def get_last_err_id():
-	cursor.execute("Select max(res_err_id) from tbl_results")
-	row = cursor.fetchone()
-	if row[0] is None:
-		return 0
-	else:
-		return row[0]
-
-## Return the last icmpext id so that we can assign the next one
-def get_last_icmpext_id():
-	cursor.execute("Select max(set_icmpext_mpls_id) from tbl_result_set")
-	row = cursor.fetchone()
-	if row[0] is None:
-		return 0
-	else:
-		return row[0]
-
-## Return the last icmpext id so that we can assign the next one
-def get_last_icmpext_mpls_id():
-	cursor.execute("Select max(mpl_icmptext_mpls_id) from tbl_traceroute_mpls")
-	row = cursor.fetchone()
-	if row[0] is None:
-		return 0
-	else:
-		return row[0]
-
-## Return the last result id so that we can assign the next one
-def ret_max_res_id():
-	cursor.execute("Select max(res_set_id) from tbl_results")
-	row = cursor.fetchone()
-	if row[0] is None:
-		return 0
-	else:
-		return row[0]
-
-## ~Depreciated~ #delete this
-## Inserts data into the database.
-def insert_into_db(tbl,tbl_schem):
-	#1: remove all None values
-	#2: create a string of all viable(non Null) keys, same with values
-	#3: insert info into db (columns,values)
-	keys = ""
-	vals = ""
-	tmp = tbl_schem
-	tbl_schem = sanitize_dict(tbl_schem)
-	for k,v in tbl_schem.iteritems():
-		keys += "'"+(k)+"',"
-		vals += "'"+str(v)+"',"
-	keys = keys[:-1]
-	vals = vals[:-1]
-	cursor.execute("insert into "+tbl+"("+(keys)+") values("+vals+")")
-
-## Inserts data into the database, a tuple(table name, values) is parsed
-def insert_tuple_in_db(inserts):
-	if type(inserts) != type(tuple):
-		print "Inserts should be in the form of a tuple. tuple : (table name, values)"
-		print 'example: ("tbl_Measurements",(111111,"date","127.0.0.1",4,"ICMP","Ping"))'
-		return
-	## to include: split executes larger than 30 000
-	for insert in inserts:
-		## Declarations
-		tbl = insert[0]
-		que = insert[1]
-		##
-		que = sanitize_dict(que)
-		if len(que) >= 1:
-			keys = ", ".join(que.keys())
-			vals = ", ".join('?' * len(que))
-			query = "insert into "+tbl+"({}) values({})".format(keys,vals)
-			cursor.execute(query,que.values())
-
-## Return the results of a specific measurement
-def get_measurements_web(measurement):
-	try:
-		urls = "https://atlas.ripe.net/api/v1/measurement/"+str(measurement)+"/result/"
-		req = urllib2.urlopen(urls)
-		req = json.load(req)
-		return req
-	except urllib2.HTTPError, e: ## <- Idea from Willem Toorop
-		print (e.read())
-
-## Return the data from the measurements file
-def get_measurements_file(fileName):
-	if len( fileName ) > 0:
-		if os.path.exists(fileName):
-			f = open(fileName,'r')
-			mes = f.readlines()
-			f.close()
-			return mes
-	else:
-		print ("Could not find the measurements file: "+str(filename)+" or the file is empty")
-		sys.exit(0)
-
-## Return the time stamp in the format yyyy-mm-dd/hh-mm-ss
-def ret_timestamp_to_dt(ts):
-	dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d/%H:%M:%S')
-	return dt
-
-## Return the last row in a table
-def get_last_row(tbl): ## Not used
-	cursor.execute('select * from '+tbl+' where oid = (select max(oid) from '+tbl+');')
-	return cursor.fetchone()
-
-## Return all items from a table
-def view_tbl(tbl):
-	row = cursor.execute("Select * from "+tbl)
-	return row.fetchall()
-
-## Gather Measurements info
-def get_measurement_info(measurement):
-	try:
-		response = urllib2.urlopen("https://atlas.ripe.net/api/v1/measurement/"+str(measurement)).read()
-		response = json.loads(response)
-		return response
-	except urllib2.HTTPError, e: ## <- Idea from Willem Toorop
-		print (e.read())
-		return None
-
-## Return a dict(column name:None)
-def get_tbl_schema(tbl):
-	d = {}
-	for r in cursor.execute("PRAGMA table_info("+tbl+");"):
-		d.update({str(r[1]):None})
-	return d
+## Anon func to determine whether a key is in a dict.
+## Lengthly name to ensure it does not clash.
+exists_in_dict = lambda x,d: True if x in d else False
 
 ## Return whether the database is up to date or not
 def check_if_updated(measurements,mes_ids):
@@ -312,30 +348,15 @@ def check_if_updated(measurements,mes_ids):
 		if fi_mes[i] not in mes_ids : to_add.append(fi_mes[i])
 	return to_add
 
-## View measurement info
-def view_measurement_info(measurement):
-	response = measurement_info(measurement)
-	if response != None:
-		## For now, this will do.
-		for k,v in response.iteritems():
-			print k,":",v
+## ---------------------------------------------------------------------------------------------------------
+## Caller methods:
+## ---------------------------------------------------------------------------------------------------------
+## These methods should be called by the user which will incorperate the use of auxiliary methods
 
-## Add measurements info to insert list. return tuple for tuple insert.
-def insert_measurement_info(mes_info):
-	tbl_Measurements = get_tbl_schema("tbl_Measurements")
-	for k,v in tbl_Measurements.iteritems():
-		val = mes_info.get(k[:4],None)
-		if val:
-			tbl_Measurements[k] = val
-	return ("tbl_Measurements",tbl_Measurements)
-
-## Method to insert measurements from file to database to inserts list
-def insert_measurements_from_file(measurements):
-	insert = []
-	return insert
-
-## Method to add single measurement to database and returns success
-def add_single_measurement_to_db(measurement,insert=1):
+## Method to add a single measurement to database and returns success
+## Optional parameter means the calling member can indicate whether to insert the 
+## query now or wait, in the case of batch queries
+def add_single_measurement_to_db(measurement,insert_measurement=1):
 	if len(str(measurement)) > 0:
 		mes_info = measurement_info(measurement)
 		if "Stopped" in mes_info['status'].values():
@@ -349,50 +370,27 @@ def add_single_measurement_to_db(measurement,insert=1):
 			## Deal with results
 			results = get_measurements_web(measurement)
 			inserts.append(db_place(results))
-			## Then insert the info
-			## Adding an optional parameter "insert" means this method can be used by other methods to insert singles
-			if insert:
+			## Judge whether to insert the query now or rather pass the insert back to the caller
+			if not insert_measurement:
+				return inserts
+			else:
 				for insert in inserts:
 					insert_tuple_in_db(inserts)
-			else:
-				return inserts
-			return True
+				con.commit()
+				con.close()
+				return True ## Success
+		## default return is false meaning the insert did not happen for various reasons
 		else:
 			print "This measurement has not yet stopped, please try again later"
 			print "May I recommend viewing the measurements info or adding the insert to  the schedule"
-			return False
+			return False ## Failure
 
-## Return all the measurement ids
-def get_all_measurement_ids():
-	ids = []
-	cursor.execute("select mes_msm_id from tbl_Measurements")
-	c = cursor.fetchall()
-	for i in c:
-		ids.append(i[0])
-	return ids
+## Method to insert measurements from file to database to inserts list
+def insert_measurements_from_file(measurements):
+	insert = []
+	return insert
 
 if __name__ == "__main__":	
-	#db_conn()
-	#fi = 'Measurements.txt'
-	#tbls = ['tbl_Measurements', 'tbl_results', 'tbl_result_set', 'tbl_traceroute_mpls']
-	#mes_ids = get_all_measurement_ids()
-	#insert_list = []
-	#measurements = get_measurements_file(fi)
-	#to_add = check_if_updated(measurements,mes_ids)
-	#if to_add != None or len(to_add) != 0:
-		#for measurement in to_add:
-			#mes_info = measurement_info(measurement)
-			#if "Stopped" in mes_info['status'].values():
-				#req = get_measurements_web(measurement)
-				#ins = db_place(req,tbls)
-				#insert_list.append(ins)
-			#else:
-				#print 'Measurement "'+str(measurement)+'" has not stopped, please try again later.'
-	#else:
-		#print "The database is up to date according to the measurements file"
-	#for x in range(len(insert_list)):
-		#if type(insert_list[x]) is not type(None):
-			#insert_tuples_in_db(insert_list[x])
 	#conn.commit()
 	print "Fin"
 	conn.close()
