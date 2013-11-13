ab4cc13208955e0f8fa632a14c09928a0bceb356
Warwick Louw <Warwickbrettlouw@gmail.com>
2013-11-13 17:04:45 +0100

    EOD commit: Scheduled ready for testing.

--- Scheduled.py
+++ Scheduled.py
@@ -5,7 +5,7 @@ import Probes
 import Database
 import itertools
 import Processing
-import Atals_Query
+import Atlas_Query
 import Database_Handler as DB_Handler
 
 class Scheduled:
@@ -14,11 +14,11 @@ class Scheduled:
 	#def submit(self,query,probes): ## This is where Atlas query comes in. Maybe improve Atlas_Query
 	def busy_probes(self):
 		## (returned as a list of dictionaries)
-		busy_probes = DB_Handler.query_table("tbl_Measurements",'targeted',' "finished" != 1')[0]['targeted']
+		busy_probes = DB_Handler.query_table("tbl_Measurements",'targeted_probes',' "finished" != 1')
 		if not busy_probes:
 			return []
 		else:
-			busy_probes = str(busy_probes)[1:-1].split(",")
+			busy_probes = str(busy_probes[0]['targeted_probes'])[1:-1].split(",")
 			return busy_probes
 	def lazy_probes(self): ## Needs to be tested.
 		## Select probes that were targeted but did not respond in the last 7 days
@@ -36,7 +36,7 @@ class Scheduled_IPv6_Capable(Scheduled):
 	def __init__(self,propety):
 		Scheduled.__init__(self,propety)
 	def process_results(self,measurements):
-		## This will assume that the field targeted has alrady been filled in.
+		## This will assume that the field targeted has already been filled in.
 		## With that said, let's get some results.
 		for measurement in measurements:
 			## Get results from the web
@@ -47,16 +47,24 @@ class Scheduled_IPv6_Capable(Scheduled):
 			targeted_probes = DB_Handler.query_table("tbl_Measurements",'"Targeted"',' "measurement_id" == '+str(measurement))
 			incapable_probes = [probe for probe in targeted_probes.split(",") if probe not in failed_probes and probe not in succeeded_probes]
 			updates = {"failed":failed_probes,"suceeded":succeeded_probes,"incapable":incapable_probes}
-			ret = DB_Handler.list_update("tbl_Measurements",updates,' "measurement_id" == '+str(measurement))
+			ret = DB_Handler.batch_update("tbl_Measurements",updates,' "measurement_id" == '+str(measurement))
 			if ret:
 				print ("Update measurement:"+str(measurement)+" successful")
 	def run(self):
 		## routine here
-		p =  Probes.ipv6()
-		p -= self.busy_probes()
-		p -= self.lazy_probes()
+		p = Probes.ipv6()
+		p = [probe for probe in p if p not in self.busy_probes()]
+		p = [probe for probe in p if p not in self.lazy_probes()]
+		print p
 		## Until I imporve Atlas_Query, this will have to do.
-		Atlas_Query.baseline_dns(p)
+		## Submit
+		measurement = Atlas_Query.baseline_dns(p)
+		## Temp store targeted probes into the database, this will be done in Atlas_Query
+		cols = ["measurement_id","network_prop","timestamp","finished","targeted_probes"]
+		vals = [str(measurement),self.propety,int(time.time()),0,p]
+		ret = DB_Handler.list_insert("tbl_Measurements",(cols,vals))
+		if ret:
+			print ("Update measurement:"+str(measurement)+" successful")
 
 if __name__ == "__main__":
 	ipv6sched = Scheduled_IPv6_Capable("ipv6Capable")
