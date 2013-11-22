41c4ceaef74f03292aa53747e3bc8440e7ee4acb
Warwick Louw <Warwick@nlnetlabs.nl>
2013-11-22 14:49:17 +0100

    Renamed Scheduled Scheduler, ipv6Capable: process,measure done ready to test.

--- Scheduler.py
+++ Scheduler.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python
 ## Timeless Scheduler
 
 import Database
 from atlas import *
 
-class Scheduled:
-	def __init__(self,prop):
-		self.propety = prop
+class Scheduler:
+	def __init__(self):
+		pass
 
-	def probes(): ## for now, no args.
+	def probes(self): ## for now, no args.
 		p = set([probe['id'] for probe in atlas.probe(prefix_v6 = '::/0', limit = 0)]) ## Dealing with the generator
 		return p
 
@@ -16,10 +17,10 @@ class Scheduled:
 		time_period = (time() - 7 * 24 * 60 * 60) ## 1 week ago
 		q = """ SELECT probe_id FROM Measurements,Targeted
 				WHERE Measurements.submitted > {week}
-				AND Measurements.finished = "None"
+				AND Measurements.finished = 0
 				AND Measurements.network_propety = '{prop}'
 				AND Measurements.measurement_id = Targeted.measurement_id
-			""".format(week = int(time_period),prop = self.propety)
+			""".format(week = int(time_period),prop = self.get_propety_name())
 		rows = cursor.execute(q).fetchall()
 
 		if not rows:
@@ -34,7 +35,7 @@ class Scheduled:
 		cursor = Database.get_con().cursor()
 		time_period = (time() - 7 * 24 * 60 * 60) ## 1 week ago
 		appearance = 5 ## The number of times a probe should appear in Targeted and have no result to be a lazy probe
-		q = """ SELECT Targeted.probe_id FROM Targeted, Measurements
+		q = """ SELECT Targeted.probe_id FROM Targeted, Measurements, Results
 				WHERE Measurements.submitted > {week}
 				AND Measurements.network_propety = '{prop}'
 				AND Measurements.measurement_id = Results.measurement_id
@@ -42,10 +43,10 @@ class Scheduled:
 				(SELECT Results.probe_id from Results, Measurements
 				WHERE Measurements.submitted > {week}
 				AND Measurements.measurement_id = Results.measurement_id
-				AND Measurements.network_prop = '{prop}')
+				AND Measurements.network_propety = '{prop}')
 				GROUP BY Targeted.probe_id
 				HAVING COUNT(Targeted.probe_id) > {appr}
-			""".format(week = int(time_period), prop = self.propety, appr = appearance)
+			""".format(week = int(time_period), prop = self.get_propety_name(), appr = appearance)
 		rows = cursor.execute(q).fetchall()
 		if not rows:
 			print("No lazy probes")
@@ -64,7 +65,7 @@ class Scheduled:
 				AND Measurements.network_propety = '{prop}'
 				GROUP BY Results.probe_id
 				HAVING COUNT(*) > {appr}
-			""".format(week = int(time_period), prop = self.propety, appr = appearance)
+			""".format(week = int(time_period), prop = self.get_propety_name(), appr = appearance)
 		rows = cursor.execute(q).fetchall()
 		if not rows:
 			print("No finished probes")
@@ -74,42 +75,51 @@ class Scheduled:
 		return done_probes
 
 	def run(self):
-		#self.measure()
+		self.measure()
 		self.process()
 
-class Scheduled_IPv6_Capable(Scheduled):
+class Scheduler_IPv6_Capable(Scheduler):
 
 	def __init__(self,propety):
-		Scheduled.__init__(self,propety)
+		self.propety_name = propety
+		Scheduler.__init__(self)
+
+	def get_propety_name(self):
+		return self.propety_name
 
 	def measure(self):
 		p = self.probes()
 		p -= self.busy_probes()
 		p -= self.lazy_probes()
-		p -= self.done_probes()	
-		defs = atlas.dns6('nl','AAA','2001:7b8:40:1:d0e1::1')
+		p -= self.done_probes()
+		defs = atlas.dns6('nl','AAAA','2001:7b8:40:1:d0e1::1')
 		probes = list(p) ## Convert to list to be used in atlas
 		response = atlas.create(defs,probes)
-		print response
-		## rewrite in progress
-		#if not measurements:
-			#print "No measurements were correctly submitted."
-			#return False
-		#con = Database.get_con()
-		#cursor = con.cursor()
-		#for measurement in measurements:
-			#q = """
-				#""".format()
-			#try:
-				#cursor.execute(q)
-			#except Exception,e:
-				#print (e)
-				#con.close()
-				#return
-		#con.commit()
-		#con.close()
-		#print ("measurement: {} successfully updated.".format(measurement))
-		#return True
+		measurements = response['measurements']
+		if not measurements:
+			print "No measurements were correctly submitted."
+			return
+		now = int(time())
+		con = Database.get_con()
+		cursor = con.cursor()
+		for measurement in measurements:
+			q = """ INSERT INTO Measurements(measurement_id,network_propety,submit,finished)
+					VALUES({msm},"{prop}",{now},"None")
+				""".format(measurement,self.get_propety_name(),now)
+			try:
+				cursor.execute(q)
+				for probe in probes:
+					pq = "INSERT INTO Targeted(measurement_id,probe_id) VALUES({},{})".format(measurement,probe)
+					try:
+						cursor.execute(pq)
+					except Exception,e:
+						print("Error inserting probe {} into Targeted.".format(probe))
+				print ("measurement: {} successfully inserted.".format(measurement))
+			except Exception,e:
+				print ("Error inserting measurement {}, reasone: {}".format(measurement,e))
+		con.commit()
+		con.close()
+		return
 
 	def process(self):
 		## Get all measurement ids that are less than a week old and are ready to process.
@@ -120,7 +130,7 @@ class Scheduled_IPv6_Capable(Scheduled):
 				WHERE submitted > {week}
 				AND network_propety = '{prop}'
 				AND finished = "None"
-			""".format(week = time_period, prop = self.propety)
+			""".format(week = time_period, prop = self.get_propety_name())
 		rows = cursor.execute(q).fetchall()
 		if not rows:
 			print("There are no measurements to process")
@@ -131,27 +141,29 @@ class Scheduled_IPv6_Capable(Scheduled):
 		for measurement in measurements:
 			response = [x for y in list(atlas.result(measurement)) for x in y]
 			for result in response:
-				probe_id = result['ID']
+				probe_id = result['prb_id']
 				## This only works for DNS.
 				good = 0 if result.get('error',None) else 1
 				## Later add a check to validate the answer.
-				q = "insert into Results(measurement_id,probe_id,good,json) values({},{},{},'{}')".format(measurement,probe_id,good,result)
+				q = 'insert into Results(measurement_id,probe_id,good,json) values({},{},{},"{}")'.format(measurement,probe_id,good,result)
+				print q
 				cursor.execute(q)
-				q = "UPDATE Measurements SET finished = 1 WHERE measurement_id = {}".format(measurement)
-				cursor.execute(q)
-		#con.commit()
-		print("Result processed")
+			q = "UPDATE Measurements SET finished = 1 WHERE measurement_id = {}".format(measurement)
+			print q
+			cursor.execute(q)
+		con.commit()
+		print("Results processed")
 		return
 
 if __name__ == "__main__":
 	## List of network propeties
 	## testing purposes
-	sch = Scheduled_IPv6_Capable("ipv6Capable")
+	sch = Scheduler_IPv6_Capable("ipv6Capable")
 	sch.run()
 	#net_props = ["ipv6Capable"]
 	#for prop in net_props:
 		#if prop == "ipv6Capable":
-			#sch = Scheduled_IPv6_Capable(prop)
+			#sch = Scheduler_IPv6_Capable(prop)
 		#try:
 			#sch.run()
 		#except Exception,e:
